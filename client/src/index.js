cea558d381cc202b72ebe7c75e165e14658f4266
Николай Смекалов <NikolaySmekalov@MBP-Nikolaj.Dlink>
2016-10-06 14:04:04 +0300

    Merge branch 'dev' of /Users/NikolaySmekalov/server/Game/htdocs with conflicts.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,6 @@
 "use strict";
-
-import DataManager from './core/DataManager';
 import Game from './core/Game';
-
-let test = document.querySelector(".js-test");
-
-
-let main = document.querySelector(".js-app");
+let main = document.querySelector(".app");
 let canvas = document.createElement("canvas");
 canvas.width = 800;
 canvas.height = 600;
@@ -14,22 +8,9 @@ canvas.className = 'game';
 
 main.appendChild(canvas);
 
-// Load data
-let dm = new DataManager();
-dm.preLoad();
-
-// start game
-let options = {fps: 60, debug: true};
+let options = {fps: 30, debug: true};
 let game = new Game(canvas, options);
 game.start();
-game.togglePause();
+//game.togglePause();
 
-let pauseBtn = document.querySelector(".js-pause");
-pauseBtn.addEventListener("click", (event) => {
-    game.togglePause();
-    if (event.target.innerHTML === "Pause") {
-        event.target.innerHTML = "Start";
-    } else {
-        event.target.innerHTML = "Pause";
-    }
-});
+

6bfabca9108518fdbba516bc2120594de587619e
Aleksey Kazantsev <develop.kaa@gmail.com>
2016-10-07 14:52:46 +0300

    config Webpack for styles, some file restructurisation

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,35 +1,48 @@
 "use strict";
 let webpack = require('webpack');
+let path = require('path');
 
 module.exports = {
+  debug: true,
+  noInfo: false,
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
     './src/index.js'
   ],
-  module: {
-    loaders: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js']
-  },
-  output: {
+    output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
+  target: 'web',
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-1']
+        }
+      },
+      {test: /\.css$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
   devServer: {
-    contentBase: './',
+    contentBase: './src',
     hot: true
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
   ]
 };

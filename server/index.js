dd57d340b29f777300a177e6cc654d58aa04167f
Aleksey Kazantsev <develop.kaa@gmail.com>
2016-10-11 22:44:54 +0300

    socket io
    get server status and some work with server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@
  * Created on 27.09.16.
  * index of server project
  */
+import makeStore from './src/store';
 import startServer from './src/server';
 
-startServer();
+export const store = makeStore();
+
+startServer(store);
+
+//store.dispatch({type: 'NEW_GAME'});
